84df9a029ae20f73dfbe9533cb065ac9571cf9d2
Ezau Martins <ezaumateus97@gmail.com>
2021-07-15 20:22:03 -0400

    CommitInicial

---
+++ src/main/java/com/projeto/AtribuicaoReferencia/Main.java
@@ -0,0 +1,11 @@
+package com.projeto.AtribuicaoReferencia;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA = " + intA +" intB = "+ intB  );
+    }
+}
