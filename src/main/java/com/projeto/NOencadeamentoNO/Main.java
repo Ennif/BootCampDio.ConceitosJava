c41f3de3330269b3ae94ce6f464ddced7f35427c
Ezau Martins <ezaumateus97@gmail.com>
2021-07-17 21:34:55 -0400

    Generics

--- src/main/java/com/projeto/NOencadeamentoNO/Main.java
+++ src/main/java/com/projeto/NOencadeamentoNO/Main.java
@@ -3,15 +3,15 @@ package com.projeto.NOencadeamentoNO;
 public class Main {
     public static void main(String[] args) {
 
-        No no1 = new No("Conteudo Primeiro Nó");
-        No no2 = new No("Conteudo Segundo Nó");
+        No<String> no1 = new No<>("Conteudo Primeiro Nó");
+        No<String> no2 = new No<>("Conteudo Segundo Nó");
 
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteudo Terceiro Nó");
+        No<String> no3 = new No<>("Conteudo Terceiro Nó");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteudo Quarto Nó");
+        No<String> no4 = new No<>("Conteudo Quarto Nó");
         no3.setProximoNo(no4);
 
         System.out.println(no1);
