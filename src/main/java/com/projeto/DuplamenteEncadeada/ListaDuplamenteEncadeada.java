57e4ad787b7fe3b2d5c2fc4a2bfab16969fad16a
Ezau Martins <ezaumateus97@gmail.com>
2021-07-22 22:25:16 -0400

    DEncadeada.AddMetodos.Get.GetNO

--- src/main/java/com/projeto/DuplamenteEncadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/DuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -7,10 +7,23 @@ public class ListaDuplamenteEncadeada<T> {
 
     private int tamanhoLista;
     ///////////////////////////////////
+
     public ListaDuplamenteEncadeada(){
         primeiroNo = null;
         ultimoNo = null;
         tamanhoLista = 0;
+    }
+
+    public T get(int value){
+        return this.getNO(value).getConteudo();
+    }
+
+    private NoDuplo<T> getNO(int value){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; (i < value)&&(noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return  noAuxiliar;
     }
 
     private int Size(){
