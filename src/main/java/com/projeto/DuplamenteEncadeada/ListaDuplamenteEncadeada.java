e73ea6cb9680eb37da1c027057fdce509bd77502
Ezau Martins <ezaumateus97@gmail.com>
2021-07-24 21:15:52 -0400

    DEncadaeadas.MetodosAdd

--- src/main/java/com/projeto/DuplamenteEncadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/DuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -17,6 +17,46 @@ public class ListaDuplamenteEncadeada<T> {
     public T get(int value){
         return this.getNO(value).getConteudo();
     }
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if (primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null){
+            ultimoNo.setNoPrevio(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
+    public void add(int index, T elemento){
+        NoDuplo<T> noAuxilar = getNO(index);
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(noAuxilar);
+
+        if (novoNo.getNoProximo() != null){
+            novoNo.setNoPrevio(noAuxilar.getNoPrevio());
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        }else{
+            novoNo.setNoProximo(ultimoNo);
+            ultimoNo = novoNo;
+        }
+
+        if (index == 0){
+            primeiroNo = novoNo;
+        }else{
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+
+        }
+
+        tamanhoLista ++;
+
+
+
+    }
+
 
     private NoDuplo<T> getNO(int value){
         NoDuplo<T> noAuxiliar = primeiroNo;
@@ -29,5 +69,6 @@ public class ListaDuplamenteEncadeada<T> {
     private int Size(){
         return tamanhoLista;
     }
+
 
 }
